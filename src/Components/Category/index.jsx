20c8900312c015b15dce45cc90838e797619e140
Ashu2806 <ashutoshsharma2897@gmail.com>
2024-08-06 21:12:38 +0530

    Icon changes in category section

--- src/Components/Category/index.jsx
+++ src/Components/Category/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BiChevronRight, BiAtom, BiDish } from "react-icons/bi";
+import { PiHamburger } from "react-icons/pi";
 import './Category.css'
 
 export default function Category() {
     return (
         <div className='category-wrapper p-5 flex flex-col justify-center gap-8'>
             <div className='category flex items-center gap-4' style={{ paddingTop: "0px" }}>
-                <div className='category-icon'>
+                <div className='category-icon' id="category-icon-1">
                     <BiAtom />
                 </div>
                 <div className='category-text flex-grow'>Goal</div>
@@ -15,8 +16,8 @@ export default function Category() {
                 </div>
             </div>
             <div className='category flex items-center gap-4'>
-                <div className='category-icon'>
-                    <BiAtom />
+                <div className='category-icon' id="category-icon-2">
+                    <PiHamburger />
                 </div>
                 <div className='category-text  text-sm md:text-base lg:text-lg flex-grow'>Popular Dishes</div>
                 <div className='category-arrow'>
